b4584eef7c8a35ec3022b5848c4039ec00887f76
Mike <42130525+vvvvtrt@users.noreply.github.com>
2024-03-10 12:46:50 +0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,10 @@
+module example/data-access
+
+go 1.21.5
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+// main.go
+package main
+
+import (
+	"fmt"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"encoding/json"
+	"net/http"
+)
+
+type person struct {
+	Name string
+	Age  int8
+}
+
+func openSQL() {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/avito")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query("INSERT INTO `microcategory` (`name`, `node`, `price`) VALUES('Телефоны', '2', 0)")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer insert.Close()
+
+	fmt.Println("Nice")
+}
+
+type Message struct {
+	Message string `json:"text"`
+}
+
+func getMessageHandler(w http.ResponseWriter, r *http.Request) {
+	message := Message{Message: "Hello from Golang backend!"}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	json.NewEncoder(w).Encode(message)
+}
+
+func main() {
+	// http.HandleFunc("/api/message/", getMessageHandler)
+
+	// http.ListenAndServe(":8000", nil)
+
+	openSQL()
+}
